0265781ed25523c7a8ead35c1f8870143d81d811
ujanth <80120140+Dalaxshan@users.noreply.github.com>
2026-05-14 16:12:23 +0530

    update main.ts to set global API prefix, configure validation pipes, and enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 5000;
+  app.setGlobalPrefix('api/v1');
+  const port = process.env.PORT || 4000;
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.enableCors();
   await app.listen(port);
-  console.log(`port ${port} connect succesfully`);
+  console.log(`Server running on http://localhost:${port}/api/v1`);
 }
 bootstrap();
