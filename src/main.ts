bb060eef26c25ae1bc92354cd55c2b1ab0ba265c
ujanth <80120140+Dalaxshan@users.noreply.github.com>
2026-06-06 10:57:11 +0530

    update main ts

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // app.use(cookieParser());
-  // app.setGlobalPrefix('api/v1');
-  // app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.use(cookieParser());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const allowedOrigins = [
-    'https://www.admasterlk.com',
+     'https://www.admasterlk.com',
     'https://admasterlk.com',
     'https://admin.admasterlk.com',
     'https://www.admin.admasterlk.com',
@@ -20,24 +20,14 @@ async function bootstrap() {
   ];
 
   app.enableCors({
-    // origin: (origin, callback) => {
-    //   if (!origin || allowedOrigins.includes(origin)) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error('Not allowed by CORS'));
-    //   }
-    // },
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://www.admasterlk.com',
-      'https://admasterlk.com',
-      'https://admin.admasterlk.com',
-      'https://www.admin.admasterlk.com',
-    ],
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    headers: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT || 4000);
