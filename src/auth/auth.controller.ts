79596547241de533fb48358ef995c2e55afc1fb0
ujanth <80120140+Dalaxshan@users.noreply.github.com>
2026-07-24 15:05:46 +0530

    Refactor environment variable usage for cookie security settings

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -44,7 +44,7 @@ export class AuthController {
     const { accessToken, refreshToken } =
       await this.authService.refreshAccessToken(userId);
 
-    const isProd ='production';
+    const isProd = process.env.NODE_ENV === 'production';
     const cookieOptions = {
       httpOnly: true,
       secure: isProd,
